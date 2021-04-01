f8fd5fdc61ca8c729caec70150bbd925eeca9bdc
Lucky Prabath <lucky@objectoneworld.com>
2021-04-01 09:42:13 +0530

    Add temporary route from sign-in to dashboard to check keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { SignInComponent } from './view/sign-in/sign-in.component';
 import { UserService } from './service/user.service';
 import { AppRoutingModule } from './app-routing.module';
 import { SignUpComponent } from './view/sign-up/sign-up.component';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
